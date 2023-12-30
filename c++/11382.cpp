02c28621772a2ce4ac00a281292c3520cbe5fbd8
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2023-12-30 13:30:48 +0900

    11382풀이

---
+++ c++/11382.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int num1, num2, num3;
+    int sum;
+
+    cout<<"숫자를 입력해주세요 : ";
+    cin>>num1>>num2>>num3;
+    sum = num1 + num2 + num3;
+    cout<<sum<<endl;
+}
