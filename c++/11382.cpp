99daed1a3655bfafdde8d42151a6d1a8c92cea88
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-01-01 21:37:04 +0900

    11382번 풀이

--- c++/11382.cpp
+++ c++/11382.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(void){
-    int num1, num2, num3;
-    int sum;
+    long long num1, num2, num3;
+    long long sum;
 
-    cout<<"숫자를 입력해주세요 : ";
     cin>>num1>>num2>>num3;
     sum = num1 + num2 + num3;
     cout<<sum<<endl;
+
+    return 0;
 }
