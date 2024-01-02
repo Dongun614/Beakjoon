7f9b286049863d9e2ddd7ff40734543cc3162a4a
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-01-02 23:49:14 +0900

    11382번 풀이수정

--- c++/11382.cpp
+++ c++/11382.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(void){
-    long long num1, num2, num3;
+    long long A, B, C;
     long long sum;
 
-    cin>>num1>>num2>>num3;
-    sum = num1 + num2 + num3;
+    cin>>A>>B>>C;
+    sum = A + B + C;
     cout<<sum<<endl;
 
     return 0;
